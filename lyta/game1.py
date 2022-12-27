6cf16f729c525d9500c959455ee2541832e2e362
lytaa123 <chhakklyta@gmail.com>
2022-12-27 13:26:47 +0700

    edit file

--- lyta/game1.py
+++ lyta/game1.py
@@ -9,10 +9,11 @@ if a == "admin":
     for question in ls:
         print(question)
 else:
-
-    print('Question1: 1+1 = (2 or 1) ')
-    x = input('Your choice is : ')
-    if x == "2":
-        print("Your answer is True")
-    else:
-        print("Your answer is False")
+    p = input('Enter Password= ')
+    if p == "123":
+        print('Question1: 1+1 = (2 or 1) ')
+        x = input('Your choice is : ')
+        if x == "2":
+            print("Your answer is True")
+        else:
+            print("Your answer is False")
