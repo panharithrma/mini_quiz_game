d06979fe10ccda3b1721344a7a1e159c73a67196
lytaa123 <chhakklyta@gmail.com>
2022-12-27 11:57:32 +0700

    quiz

---
+++ lyta/game1.py
@@ -0,0 +1,18 @@
+print('Welcome to Mini Game')
+a = input('Enter Username= ')
+if a == "admin":
+    ls = [
+        'Question1: ',
+        'Question2: ',
+        'Question3: ',
+    ]
+    for question in ls:
+        print(question)
+else:
+
+    print('Question1: 1+1 = (2 or 1) ')
+    x = input('Your choice is : ')
+    if x == "2":
+        print("Your answer is True")
+    else:
+        print("Your answer is False")
